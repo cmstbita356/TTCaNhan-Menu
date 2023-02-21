649b160815018b37a4454376cf96855f824c0966
cmstbita356 <cmstbita356@gmail.com>
2023-02-21 09:20:35 +0700

    .

--- TTCaNhan/app/src/main/java/com/example/ttcanhan/DisplayActivity.java
+++ TTCaNhan/app/src/main/java/com/example/ttcanhan/DisplayActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class DisplayActivity extends AppCompatActivity {
@@ -36,5 +38,13 @@ public class DisplayActivity extends AppCompatActivity {
 
         TextView textView_amnhac = findViewById(R.id.textView_amnhac);
         textView_amnhac.setText(bundle.getString("amnhac"));
+
+        Button button_return = findViewById(R.id.button_return);
+        button_return.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
